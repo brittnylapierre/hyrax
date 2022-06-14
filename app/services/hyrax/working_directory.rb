f8042047040090699061caec9597eb60cdfab563
conorom <conorom@umich.edu>
2022-06-14 17:35:44 -0400

    always pull repo file if filepath param is missing

--- app/services/hyrax/working_directory.rb
+++ app/services/hyrax/working_directory.rb
@@ -3,23 +3,6 @@ module Hyrax
   # @deprecated Use JobIoWrapper instead
   class WorkingDirectory
     class << self
-      # Returns the file passed as filepath if that file exists. Otherwise it grabs the file from repository,
-      # puts it on the disk and returns that path.
-      # @param [String] repository_file_id identifier for Hydra::PCDM::File
-      # @param [String] id the identifier of the FileSet
-      # @param [String, NilClass] filepath path to existing cached copy of the file
-      # @return [String] path of the working file
-      def find_or_retrieve(repository_file_id, id, filepath = nil)
-        return filepath if filepath && File.exist?(filepath)
-        repository_file = Hydra::PCDM::File.find(repository_file_id)
-        working_path = full_filename(id, repository_file.original_name)
-        if File.exist?(working_path)
-          Rails.logger.debug "#{repository_file.original_name} already exists in the working directory at #{working_path}"
-          return working_path
-        end
-        copy_repository_resource_to_working_directory(repository_file, id)
-      end
-
       # @param [#original_name, #id] file the resource in the repo
       # @param [String] id the identifier of the FileSet
       # @return [String] path of the working file
